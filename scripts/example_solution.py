e8edbe6e6f38f5a87e16dc384f34901398611bcd
Ruben Troch <ruben.troch@student.kuleuven.be>
2020-04-25 17:12:38 +0200

    Before Pulling Update

--- scripts/example_solution.py
+++ scripts/example_solution.py
@@ -6,12 +6,48 @@ As you can see, a solution contains python functions and nothing else.
 
 
 def query_01(connection, column_names):
-    return None
-
+    # Bouw je query
+    query="""
+    select    name, yearID, HR
+    from      Teams
+    order by  HR DESC;
+    """
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
 
 def query_02(connection, column_names, datum_x="1980-01-16"):
-    return None
-
+    # Bouw je query
+    query="""
+    select    nameFirst, nameLast, birthYear, birthMonth, birthDay
+    from      MASTER
+    where     debut > '{}'
+    order by  nameLast ASC;
+    """.format(
+        datum_x
+    )
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
 
 def query_03(connection, column_names):
-    return None
+    # Bouw je query
+    query="""
+    select    p.nameFirst, p.nameLast, t.name
+    from      Managers as m NATURAL JOIN MASTER as p NATURAL JOIN Teams as t
+    where     m.plyrMgr = 'Y'
+    group by  t.teamID, p.playerID;
+    order by  t.name ASC;
+    """
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
